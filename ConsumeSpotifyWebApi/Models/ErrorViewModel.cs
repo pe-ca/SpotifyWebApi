e72041019094f49e38aeacf2d35f567a35763940
Ricardo Perales Cabrera <mr.gloser1@gmail.com>
2022-07-17 19:16:27 -0500

    creacion de proyecto ASP.net Core MVC

---
+++ ConsumeSpotifyWebApi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConsumeSpotifyWebApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
