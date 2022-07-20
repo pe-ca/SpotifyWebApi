a1ce8e47aedea574e95216db966faa751afa7679
Ricardo Perales Cabrera <mr.gloser1@gmail.com>
2022-07-19 21:19:42 -0500

    correccion en url de consumo

--- ConsumeSpotifyWebApi/Program.cs
+++ ConsumeSpotifyWebApi/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddHttpClient<ISpotifyAccountService, SpotifyAccountService>(c => c.BaseAddress = new Uri("https://accounts.spotify.com/"));
+builder.Services.AddHttpClient<ISpotifyAccountService, SpotifyAccountService>(c => c.BaseAddress = new Uri("https://accounts.spotify.com/api/"));
 
 var app = builder.Build();
 
