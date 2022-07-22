15496339078c866b3cca7f15415b76917cc9c790
Ricardo Perales Cabrera <mr.gloser1@gmail.com>
2022-07-21 21:59:45 -0500

    refacrotizacion de metodo

--- ConsumeSpotifyWebApi/Controllers/HomeController.cs
+++ ConsumeSpotifyWebApi/Controllers/HomeController.cs
@@ -9,24 +9,36 @@ namespace ConsumeSpotifyWebApi.Controllers
     {
         private readonly ISpotifyAccountService _spotifyAccountService;
         private readonly IConfiguration _configuration;
+        private readonly ISpotifyService _spotifyService;
 
-        public HomeController(ISpotifyAccountService spotifyAccountService, IConfiguration configuration)
+        public HomeController(ISpotifyAccountService spotifyAccountService, IConfiguration configuration, ISpotifyService spotifyService)
         {
             _spotifyAccountService = spotifyAccountService;
             _configuration = configuration;
+            _spotifyService = spotifyService;
         }
 
         public async Task<IActionResult> Index()
         {
+            var releases = await GetReleases();
+            return View();
+        }
+
+        private async Task<IEnumerable<Releases>> GetReleases()
+        {
             try
             {
                 var token = await _spotifyAccountService.GetToken(_configuration["Spotify:ClientId"], _configuration["Spotify:ClientSecret"]);
+
+                var newReleases = await _spotifyService.GetNewReleases("MX", 20, token);
+
+                return newReleases;
             }
             catch (Exception ex)
             {
                 Debug.WriteLine(ex);
+                return Enumerable.Empty<Releases>();
             }
-            return View();
         }
 
         public IActionResult Privacy()
